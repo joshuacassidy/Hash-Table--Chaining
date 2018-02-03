1ff927aa172c9cc1c2026879c5e555dd2bcfa935
joshuacassidy <Joshcassidy79@gmail.com>
2018-02-03 15:29:18 +0000

    Added unit tests

---
+++ HashTableTest.java
@@ -0,0 +1,65 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class HashTableTest {
+
+    HashTable<Integer, Integer> hashTable;
+
+    @Before
+    public void setUp() {
+        hashTable = new HashTable<>();
+    }
+
+    @org.junit.Test
+    public void get() throws Exception {
+        hashTable.put(1,121);
+        assertEquals(121, ((int) hashTable.get(1)));
+        hashTable.put(1,122);
+        assertEquals(122, ((int) hashTable.get(1)));
+        hashTable.put(2,123);
+        assertEquals(123, ((int) hashTable.get(2)));
+        hashTable.put(3,124);
+        assertEquals(124, ((int) hashTable.get(3)));
+        hashTable.put(5,125);
+        assertEquals(125, ((int) hashTable.get(5)));
+    }
+
+
+    @org.junit.Test
+    public void remove() throws Exception {
+        hashTable.put(1,121);
+        assertEquals(121, ((int) hashTable.remove(1)));
+        hashTable.put(1,122);
+        assertEquals(122, ((int) hashTable.remove(1)));
+        hashTable.put(2,123);
+        assertEquals(123, ((int) hashTable.remove(2)));
+        hashTable.put(3,124);
+        assertEquals(124, ((int) hashTable.remove(3)));
+        hashTable.put(5,125);
+        assertEquals(125, ((int) hashTable.remove(5)));
+    }
+
+    @org.junit.Test
+    public void getSize() throws Exception {
+        assertEquals(0, hashTable.getSize());
+        hashTable.put(1,1);
+        hashTable.put(1,2);
+        hashTable.put(2,2);
+        assertEquals(2, hashTable.getSize());
+    }
+
+    @org.junit.Test
+    public void isEmptyOnCreation() throws Exception {
+        assertEquals(true, hashTable.isEmpty());
+        hashTable.put(1,1);
+        assertEquals(false, hashTable.isEmpty());
+    }
+
+    @org.junit.Test
+    public void isNotEmptyWhenItemAdded() throws Exception {
+        hashTable.put(1,1);
+        assertEquals(false, hashTable.isEmpty());
+    }
+
+}
